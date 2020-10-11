d006c4fd389a57d8f6fda8ae7f59563da937ff00
Min Woo Kang <min.woo.kang@duke.edu>
2020-10-11 16:24:11 +0900

    Updates

---
+++ precache-manifest.c83d44239c592ff776bf7c900922b4e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83cf66a01a35f72519cca15cbdc4cd9e",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "5c583ebcd1860cb21362",
+    "url": "/movie_app_2020/static/css/main.61d9b31f.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c583ebcd1860cb21362",
+    "url": "/movie_app_2020/static/js/main.e4e88c88.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
