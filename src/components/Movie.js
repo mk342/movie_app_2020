9d51c45df578cb5f768b8c099e135fe79babf45e
Min Woo Kang <min.woo.kang@duke.edu>
2020-10-12 15:22:47 +0900

    Finished Routing

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,22 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({id, year, title, summary, poster, genres}) {
     return (
     <div className = "movie">
+        <Link to={{
+        pathname: `/movie/${id}`,
+        state: {
+            id,
+            year,
+            title,
+            summary,
+            poster,
+            genres
+        }
+        }}>
         <img src = {poster} alt = {title} title = {title} />
         <div className="movie__data">
             <h3 className = "movie__title">{title}</h3>
@@ -13,6 +25,7 @@ function Movie({id, year, title, summary, poster, genres}) {
                 {genres.map((genre,index) => <li key = {index} className = "genres__genre">{genre}</li>)}</ul>
             <p className = "movie__summary">{summary.slice(0, 140)}...</p>
         </div>
+      </Link>
     </div>
     )
 }
